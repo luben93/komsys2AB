0c162a77d0b70dd433e96ef78babff081a29d01c
luben <luben93@gmail.com>
2015-10-07 11:02:09 +0200

    init client

---
+++ lab2a/src/com/client/client.java
@@ -0,0 +1,10 @@
+package com.client;
+
+/**
+ * Created by luben on 2015-10-07.
+ */
+public class client {
+    public static void main(String[] args){
+        System.out.println("I AM CLIEEENT !!!!!!");
+    }
+}
