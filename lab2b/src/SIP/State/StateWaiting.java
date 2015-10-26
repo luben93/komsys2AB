9c8156996f7131c9d1a8f57a702a76ce87c088f8
juthu <thunjulia@gmail.com>
2015-10-26 20:56:04 +0100

    fix med answer not answer setsotimeout och tre incoming calls

--- lab2b/src/SIP/State/StateWaiting.java
+++ lab2b/src/SIP/State/StateWaiting.java
@@ -2,6 +2,7 @@ package SIP.State;
 
 import SIP.AudioStreamUDP;
 import SIP.SIPHandler;
+import start.SIPthread;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,6 +16,13 @@ import java.util.Scanner;
  */
 public class StateWaiting extends State {
 
+    int port_peer = -1;
+    int port = -1;
+    BufferedReader in;
+    PrintWriter out;
+    InetAddress ip;
+    AudioStreamUDP asu;
+    SIPthread server;
 
     @Override
     public SIPHandler.StateEvent getStateName() {
@@ -22,45 +30,70 @@ public class StateWaiting extends State {
     }
 
     @Override
-    public State toAnswer(BufferedReader in, PrintWriter out, InetAddress ip,AudioStreamUDP asu) throws StateException {
+    public State toNotAnswer(BufferedReader in, PrintWriter out, Socket socket) throws StateException {
         try {
+            in.close();
+            out.close();
+            socket.close();
+          //  System.out.println("type IP to call\nor 0 to exit");
+            return this;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new StateException("IO error, NOT RECEIVED INVITE, FROM STATE WAITING TO STATE ANSWER");
+        }
 
-            Scanner scanner = new Scanner(System.in);
-            String msg = in.readLine();
-            System.out.println(msg);
-            if (msg.startsWith("INVITE")) {
-                System.out.println("You have got an incoming call from " + ip.getHostName() + " do you want to answer? ");
-                System.out.println("y or n ?");
-                String s = scanner.nextLine();
-                if(s.equals("y")){
-                    int port_peer = Integer.parseInt(msg.substring(7));
-                    int port = asu.getLocalPort();
-                    out.println("100 TRYING " + port);
-                    System.out.println("100");
-                    asu.connectTo(ip, port_peer);
-                    out.println("180 RINGING");
-                    System.out.println("180");
-                    asu.startStreaming();
-                    out.println("200 OK");
-                    System.out.println("200");
-                    return new StateAnswer();
-                }else{
-                    in.close();
-                    out.close();
-                    System.out.println("type IP to call\nor 0 to exit");
-                    return new StateWaiting();
-                }
-            }
-            throw new StateException(msg + ", NOT RECEIVED INVITE, FROM STATE WAITING TO STATE ANSWER");
+    }
+
+    @Override
+    public State toAnswer(BufferedReader in, PrintWriter out, InetAddress ip, AudioStreamUDP asu, SIPthread server) throws StateException {
+        try {
+            out.println("100 TRYING " + port);
+            System.out.println("100");
+            asu.connectTo(ip, port_peer);
+            out.println("180 RINGING");
+            System.out.println("180");
+            asu.startStreaming();
+            out.println("200 OK");
+            System.out.println("200");
+            return new StateAnswer();
         } catch (IOException e) {
             e.printStackTrace();
             throw new StateException("IO error, NOT RECEIVED INVITE, FROM STATE WAITING TO STATE ANSWER");
         }
     }
 
+    @Override
+    public State toWaitForAnswer(BufferedReader in, PrintWriter out, InetAddress ip, AudioStreamUDP asu, SIPthread server) throws StateException {
+
+        try {
+
+            Scanner scanner = new Scanner(System.in);
+            String msg = in.readLine();
+            System.out.println(msg);
+
+            if (msg.startsWith("INVITE")) {
+                System.out.println("You have got an incoming call from " + ip.getHostName() + " do you want to answer? ");
+                System.out.println("y or n ?");
+                port_peer = Integer.parseInt(msg.substring(7));
+                port = asu.getLocalPort();
+            }
+            return this;
+            //throw new StateException(msg + ", NOT RECEIVED INVITE, FROM STATE WAITING TO STATE ANSWER");
+        } catch (
+                IOException e
+                )
+
+        {
+            e.printStackTrace();
+            throw new StateException("IO error, NOT RECEIVED INVITE, FROM STATE WAITING TO STATE ANSWER");
+        }
+
+    }
+
 
     @Override
-    public State toDial(PrintWriter out,AudioStreamUDP asu) {
+    public State toDial(PrintWriter out, AudioStreamUDP asu) {
+
 //        while (true) {
 //        Scanner scanner = new Scanner(System.in);
 //            System.out.println("type the IP to call\n or 0 to exit");
