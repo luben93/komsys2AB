9462da07dc113356c2d3b02557c0f7f3224841de
luben <luben93@gmail.com>
2015-10-27 12:25:04 +0100

    socket timeout execp

--- lab2b/src/SIP/State/StateTalking.java
+++ lab2b/src/SIP/State/StateTalking.java
@@ -6,6 +6,7 @@ import SIP.SIPHandler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.SocketTimeoutException;
 
 /**
  * Created by Julia on 2015-10-16.
@@ -18,26 +19,35 @@ class StateTalking extends State {
 
     @Override
     public State toWait(BufferedReader in, PrintWriter out, AudioStreamUDP asu) throws StateException {
-        try {
-            String msg = in.readLine();
-            System.out.println(msg);
-            if (msg.equals("BYE")) {
-                asu.stopStreaming();
-                out.println("200 OK");
-//                asu.close();
-                return new StateWaiting();
-            } else if (msg.equals("200 OK")) {
-                asu.stopStreaming();
-                return new StateWaiting();
-            }
+        String msg = "";
 
-            throw new StateException(msg + ", NOT RECEIVED BYE, FROM STATE TALKING TO STATE WAITING");
-        } catch (IOException e) {
+            try {
+                while (true) {
+
+                try {
+                    msg = in.readLine();
+                } catch (SocketTimeoutException e) {
+
+                }
+                System.out.println(msg);
+                if (msg.equals("BYE")) {
+                    asu.stopStreaming();
+                    out.println("200 OK");
+//                asu.close();
+                    return new StateWaiting();
+                } else if (msg.equals("200 OK")) {
+                    asu.stopStreaming();
+                    return new StateWaiting();
+                }
+            }
+//            throw new StateException(msg + ", NOT RECEIVED BYE, FROM STATE TALKING TO STATE WAITING");
+        }catch(IOException e){
             System.err.println(e.getMessage() + "IO execp, NOT RECEIVED BYE, FROM STATE TALKING TO STATE WAITING");
             asu.stopStreaming();
 //            throw new StateException("IO execp, NOT RECEIVED BYE, FROM STATE TALKING TO STATE WAITING");
             return new StateWaiting();
         }
+
     }
 
     @Override
